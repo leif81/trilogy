d73994439861d89aa0f371d8c78df06cb6520c71
leifer <leifer@311aa53a-5b27-0410-8fc2-f7950f20c8a2>
2007-01-22 04:12:40 +0000

    - get_files returns files in sorted order - fixed memory leak in get_files

--- src/list_files.cc
+++ src/list_files.cc
@@ -6,6 +6,8 @@
 
 #include <stdio.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include <dirent.h>
 #include <string.h>
 #include <errno.h>
@@ -15,8 +17,42 @@
 
 #include "list_files.h"
 
+#include <algorithm>
+
+#include <ctype.h>
+
+
 using namespace std;
 
+struct lowercase : public unary_function<char, void> 
+{
+	void operator() (char & c) 
+	{
+		c = tolower(c);
+	}
+};
+
+bool string_cmp( string a, string b )
+{
+	for_each( a.begin(), a.begin() + a.length(), lowercase() ); 	
+	for_each( b.begin(), b.begin() + b.length(), lowercase() ); 	
+
+	return a < b;
+}
+
+bool isdirectory( const string & path) 
+{
+	struct stat statbuf;
+
+	if( stat( path.c_str(), &statbuf) == -1)
+	{
+		return false;
+	}
+	else
+	{
+		return S_ISDIR(statbuf.st_mode);
+	}
+}
 
 vector<string> get_files( const string & dir_path )
 {
@@ -35,7 +71,8 @@ vector<string> get_files( const string & dir_path )
 	{
 		// check if it's a file or directory
 		const string name = dit->d_name;
-		if( opendir( name.c_str() ) == NULL )
+
+		if( !isdirectory(name) )
 		{
 			files.push_back( dir_path + "/" + name );	
 		}
@@ -46,12 +83,15 @@ vector<string> get_files( const string & dir_path )
 		//const string err = strerror[errno];
 		throw string("error closing directory");
 	}
+
+	sort( files.begin(), files.end(), string_cmp );
 	
 	return files; // return a copy, meh who cares
 }
 
+#ifdef TEST_LIST_FILES
 
-int test_main( int argc, char ** argv)
+int main( int argc, char ** argv)
 {
 	if( argc < 2 )
 	{
@@ -59,5 +99,15 @@ int test_main( int argc, char ** argv)
 		return -1;
 	}
 
+	vector<string> files = get_files( argv[1] );
+
+	vector<string>::const_iterator it = files.begin();
+	for( ; it < files.end(); ++it )
+	{
+		cout << *it << endl;
+	}
+
 	return 0;
 }
+
+#endif
