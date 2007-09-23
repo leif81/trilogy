8100b85261f97bfea038e209e58cb4a7c3da0090
leifer <leifer@311aa53a-5b27-0410-8fc2-f7950f20c8a2>
2007-09-23 21:30:43 +0000

    added gpl header

--- src/clutter-layout.c
+++ src/clutter-layout.c
@@ -1,3 +1,21 @@
+/* 
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License 
+ * as published by the Free Software Foundation; either 
+ * version 2 of the License, or (at your option) any later 
+ * version.
+ *  
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+
 #include <clutter/clutter.h>
 #include <glib.h>
 
@@ -20,7 +38,7 @@ void input_cb (ClutterStage *stage, ClutterEvent *event, gpointer data)
 
 		if (e)
 		{
-//			clutter_actor_hide (e);
+			//			clutter_actor_hide (e);
 		}
 
 	}
@@ -38,7 +56,7 @@ void input_cb (ClutterStage *stage, ClutterEvent *event, gpointer data)
 			case CLUTTER_q:
 				clutter_main_quit ();
 				break;
-			
+
 			case CLUTTER_f:
 				if( fullscreen )
 				{
